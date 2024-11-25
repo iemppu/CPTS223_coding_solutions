704df0e78ea99116f54feb14ee4fc5cd89ee09fa
iemppu <yanyan.tju@gmail.com>
2024-11-25 02:43:53 -0800

    Update main.cpp: remove unnecessary functions log2 (which caused error when compiling in Windows)

--- main.cpp
+++ main.cpp
@@ -13,34 +13,6 @@ Your answer:
 #include <random>
 #include "BST.h"
 
-double log2(double d)
-{
-    return log(d) / log(2); // log() use e as base
-}
-
-void insertRandomIntegers(BST<int>* root, int numIntegers)
-{
-    // The range of random integers
-    const int minValue = -1000000000;
-    const int maxValue =  1000000000;
-    int randomInteger;
-
-    // Generate a uniform distribution to generate random integers
-    random_device dev;
-    mt19937 rng(dev());
-    uniform_int_distribution<std::mt19937::result_type> dist(minValue, maxValue); 
-
-    // Generate random integers (without duplicates) from the specified range
-    for (int i = 0; i < numIntegers; ++i) {
-        randomInteger = dist(rng);
-        while ( root->contains(randomInteger) == true )
-        {
-            randomInteger = dist(rng);
-        }
-        root->insert(randomInteger);
-    }
-}
-
 int main()
 {
     // Seed the random number generator with the current time
