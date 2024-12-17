20757bfd45d0fca6044994492772cf815657100c
Yan Yan <yanyan.tju@gmail.com>
2024-12-17 00:52:10 -0800

    Update queue.h

--- queue.h
+++ queue.h
@@ -7,7 +7,7 @@ using namespace std;
 #define SIZE 10
 
 // Class for queue
-class queue
+class Queue
 {
 	int* arr;		// array to store queue elements
 	int capacity;	// maximum capacity of the queue
@@ -28,7 +28,7 @@ public:
 };
 
 // Constructor to initialize queue
-queue::queue(int size)
+Queue::queue(int size)
 {
 	arr = new int[size];
 	capacity = size;
@@ -38,14 +38,14 @@ queue::queue(int size)
 }
 
 // Destructor to free memory allocated to the queue
-queue::~queue()
+Queue::~queue()
 {
 	delete arr; // you are not required to test this function;
 	            // however, are there issues with it?
 }
 
 // Utility function to remove front element from the queue
-void queue::dequeue()
+void Queue::dequeue()
 {
 	// check for queue underflow
 	if (isEmpty())
@@ -62,7 +62,7 @@ void queue::dequeue()
 }
 
 // Utility function to add an item to the queue
-void queue::enqueue(int item)
+void Queue::enqueue(int item)
 {
 	// check for queue overflow
 	if (isFull())
@@ -79,7 +79,7 @@ void queue::enqueue(int item)
 }
 
 // Utility function to return front element in the queue
-int queue::peek()
+int Queue::peek()
 {
 	if (isEmpty())
 	{
@@ -90,19 +90,19 @@ int queue::peek()
 }
 
 // Utility function to return the size of the queue
-int queue::size()
+int Queue::size()
 {
 	return count;
 }
 
 // Utility function to check if the queue is empty or not
-bool queue::isEmpty()
+bool Queue::isEmpty()
 {
 	return (size() == 0);
 }
 
 // Utility function to check if the queue is full or not
-bool queue::isFull()
+bool Queue::isFull()
 {
 	return (size() == capacity);
 }
