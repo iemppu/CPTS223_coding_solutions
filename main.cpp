4236ac1c4057f9fdf5d56f786dc3261f38718695
Yan Yan <yanyan.tju@gmail.com>
2024-12-17 20:34:22 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -174,7 +174,7 @@ int main(int argc, char** argv)
 			}
 			else {
 				std::cerr << "Key does not exist in the map!" << std::endl;
-    			return EXIT_FAILURE;
+    			// return EXIT_FAILURE;
 			}
 		    // assert( it != mapByUserName.end() );
 				
