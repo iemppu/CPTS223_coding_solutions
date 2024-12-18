2d141dbba28d7ecaa5cc4d2468c9d9efb43ce972
Yan Yan <yanyan.tju@gmail.com>
2024-12-18 15:40:10 -0800

    Update test_queue.cpp

--- test_queue.cpp
+++ test_queue.cpp
@@ -4,8 +4,6 @@
 #include <cassert>
 
 void testSize_QSC(void) {
-    std::cout << "Running Test: Check size of queue - QSC" << std::endl;
-    
     // Step 1: Construct an empty queue object
     MyQueue q;
 
@@ -33,26 +31,18 @@ void testSize_QSC(void) {
     // Step 8: Verify that Queue::size() returns 1.  
     assert(q.size() == 1);
     std::cout << "Step 8 Passed: Queue size is 1 after dequeuing one element" << std::endl;
-
-    std::cout << "Test Passed: Check size of queue - QSC" << std::endl;
 }
 
 void testEmpty_EQC(void) {
-    std::cout << "Running Test: Empty queue check - EQC" << std::endl;
-
     // Step 1: Construct an empty queue object
     MyQueue q;
     
     // Step 2: Call Queue::isEmpty() to verify that Queue::isEmpty() returns 1
     assert(q.isEmpty() == 1);
     std::cout << "Step 2 Passed: Queue is empty" << std::endl;
-
-    std::cout << "Test Passed: Empty queue check - EQC" << std::endl;
 }
 
 void testFull_FQC(void){
-    std::cout << "Running Test: Full queue check - FQC" << std::endl;
-    
     // Step 1: Construct an empty queue object
     MyQueue q;
     
@@ -64,13 +54,9 @@ void testFull_FQC(void){
     // Step 3: Call Queue::isFull() to verify that queue:isFull() returns 1
     assert(q.isFull() == 1);
     std::cout << "Step 2 Passed: Queue is full" << std::endl;
-
-    std::cout << "Test Passed: Full queue check - FQC" << std::endl;
 }
 
 void testEnqueue_ENC1(void) {
-    std::cout << "Running Test: Enqueue check - ENC1" << std::endl;
-
     // Step 1: Construct an empty queue object
     MyQueue q;
     
@@ -95,14 +81,10 @@ void testEnqueue_ENC1(void) {
     // Step 7: Call Queue::peek() to verify if it returns the front element (10).  
     assert(q.peek() == 10);
     std::cout << "Step 7 Passed: Queue front is 10" << std::endl;
-
-    std::cout << "Test Passed: Enqueue check - ENC1" << std::endl;
 }
 
 
 void testEnqueueFull_ENC2(void) {
-    std::cout << "Running Test: Enqueue check (full queue) - ENC2" << std::endl;
-
     // Step 1: Construct an empty queue object
     MyQueue q;
     
@@ -121,14 +103,10 @@ void testEnqueueFull_ENC2(void) {
     // Step 5: Call Queue::size() to verify that the return value by Queue::size() equals 10 (both step 3 and 5)
     assert(q.size() == 10);
     std::cout << "Step 5 Passed: Queue size is 10" << std::endl;    
-
-    std::cout << "Test Passed: Enqueue check (full queue) - ENC2" << std::endl;
 }
 
 
 void testDequeue_DQC1(void){
-    std::cout << "Running Test: Dequeue check - DQC1" << std::endl;
-
     // Step 1: Construct an empty queue object
     MyQueue q;
     // Step 2: Call Queue::enqueue() 3 times ([0, 1, 2])
@@ -146,14 +124,10 @@ void testDequeue_DQC1(void){
     // Step 5: Call Queue::peek() to verify that Queue::peek() returns 1
     assert(q.peek() == 1);
     std::cout << "Step 5 Passed: Queue front is 1" << std::endl;   
-
-    std::cout << "Test Passed: Dequeue check - DQC1" << std::endl;
 }
 
 
 void testDequeueEmpty_DQC2(void){
-    std::cout << "Running Test: Dequeue check (empty queue) - DQC2" << std::endl;
-    
     // Step 1: Construct an empty queue object
     MyQueue q;
     
@@ -162,14 +136,10 @@ void testDequeueEmpty_DQC2(void){
     
     // Step 3: Call Queue::size() to verify that Queue::size() returns 0
     assert(q.size() == 0);
-    
-    std::cout << "Test Passed: Dequeue check (empty queue) - DQC2" << std::endl;
 }
 
 
 void testPeek_PQC1(void){
-    std::cout << "Running Test: peek queue check - PQC1" << std::endl;
-    
     // Step 1. Construct an empty Queue object
     MyQueue q;
     
@@ -181,20 +151,14 @@ void testPeek_PQC1(void){
     // Step 3. Call Queue::peek() to verify that Queue::peek() returns 0
     assert(q.peek() == 0);
     std::cout << "Step 3 Passed: Queue front is 0" << std::endl;
-    
-    std::cout << "Test Passed: peek queue check - PQC1" << std::endl;
 }
 
 
 void testPeekEmpty_PQC2(void){
-    std::cout << "Running Test: peek queue check (empty queue) - PQC2" << std::endl;
-
     // Step 1. Construct an empty queue object
     MyQueue q;
     
     // Step 2. Call Queue::peek() to verify that Queue::peek() returns std::numeric_limits<int>::min()
     assert(q.peek() == std::numeric_limits<int>::min());
     std::cout << "Step 2 Passed: Queue front is std::numeric_limits<int>::min()" << std::endl;
-    
-    std::cout << "Test Passed: peek queue check (empty queue) - PQC2" << std::endl;
 }
